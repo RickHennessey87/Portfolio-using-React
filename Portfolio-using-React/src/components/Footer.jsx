cdbd2c39117595dd5803ab65e381bff807126b56
Patrick Hennessey <pjhennessey89@gmail.com>
2024-10-14 14:03:55 -0500

    Created Header, Footer and Home Compnents

---
+++ Portfolio-using-React/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Patrick Justus Hennessey</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ Portfolio-using-React/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section>
+            <h2>Welcome to My Portfolio</h2>
+            <p>Main Content</p>
+        </section>
+    );
+}
+
+export default Home;
