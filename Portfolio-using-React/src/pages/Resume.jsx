31ae9c103eac5b89238177a705c02ea93b15cdee
Patrick Hennessey <pjhennessey89@gmail.com>
2024-10-17 14:41:49 -0500

    Added Resume and skills

--- Portfolio-using-React/src/pages/Resume.jsx
+++ Portfolio-using-React/src/pages/Resume.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import resume from '../assets/pdf/PatrickJustusHennesseyResume.pdf'
 
 function Resume() {
+    const proficiencies = [
+        'HTML', 
+        'CSS',
+        'JavaScript',
+        'Python',
+        'jQuery',
+        'Bootstrap',
+        'React',
+        'Responsive Design',
+        'Node.js',
+        'Express.js',
+        'MongoDB',
+        'Mongoose',
+        'SQL',
+        'MySQL',
+        'Sequelize',
+        'GraphQL',
+        'noSQL',
+        'Git',
+        'npm',
+        'REST APIs',
+        'AJAX',
+        'Testing',
+        'Database Theory',
+        'Algorithms',
+        'Data Structures',
+        'Project Management',
+        'AI'
+    ]
+
     return (
-        <section>
+        <section className='resume'>
             <h2>Resume</h2>
-            <p>Resume content.</p>
+            <p>
+                <a href={resume} download>
+                    Download my resume
+                </a>
+            </p>
+            <div className='proficiences'>
+                <h3>Proficiences</h3>
+                <ul>
+                    {proficiencies.map((skill, index) => (
+                        <li key={index}>{skill}</li>
+                    ))}
+                </ul>
+            </div>
         </section>
     );
 }
