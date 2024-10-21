b0799d0880f83dad2849687f5dc44cb04ee66e48
Patrick Hennessey <pjhennessey89@gmail.com>
2024-10-21 13:39:19 -0500

    Added a color palette

--- Portfolio-using-React/src/components/Footer.jsx
+++ Portfolio-using-React/src/components/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa';
 
 function Footer() {
     return (
         <footer>
+            <div className='social-icons'>
+                <a
+                    href='https://github.com/RickHennessey87'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='Github'
+                >
+                    <FaGithub />
+                </a>
+                <a
+                    href='https://www.linkedin.com/in/patrick-hennessey-1a409220b/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='LinkedIn'
+                >
+                    <FaLinkedin />
+                </a>
+            </div>
             <p>&copy; {new Date().getFullYear()} Patrick Justus Hennessey</p>
         </footer>
     );
